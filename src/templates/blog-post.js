0cb320503038628509deaacc8e56ab79321fb4d3
Arisa <ars.n.277@gmail.com>
2021-05-25 22:38:07 +0900

    settings

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,12 +4,21 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Image from "gatsby-image"
 
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const { previous, next } = data
+  const thumbnail_image = post.frontmatter.thumbnail
+
+  if (post.frontmatter.thumbnail){
+    const thumbnail_image=post.frontmatter.thumbnail.childImageSharp.fluid;
+}
+else{
+    const thumbnail_image=data.def_image.childImageSharp.fixed;
+}
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -26,6 +35,8 @@ const BlogPostTemplate = ({ data, location }) => {
         <header>
           <h1 itemProp="headline">{post.frontmatter.title}</h1>
           <p>{post.frontmatter.date}</p>
+          <Image fluid={thumbnail_image} />
+          ???
         </header>
         <section
           dangerouslySetInnerHTML={{ __html: post.html }}
@@ -79,11 +90,25 @@ export const pageQuery = graphql`
         title
       }
     }
+    def_image: file(absolutePath: { regex: "./default.jpg" }) {
+      childImageSharp {
+        fixed(width: 400, height: 300) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
     markdownRemark(id: { eq: $id }) {
       id
       excerpt(pruneLength: 160)
       html
       frontmatter {
+        thumbnail {
+          childImageSharp {
+            fluid(maxWidth: 1280) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         title
         date(formatString: "MMMM DD, YYYY")
         description
